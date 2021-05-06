8d4ef82d8e770b4bef61709567757260d5a81383
0xLegendKiller <60841283+0xLegendKiller@users.noreply.github.com>
2021-05-06 22:55:57 +0530

    Create Roman_To_Integer.py

---
+++ LeetCode/Easy/Roman_To_Integer.py
@@ -0,0 +1,26 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        change = {
+            'I': '1',
+            'V': '5',
+            'X': '10',
+            'L': '50',
+            'C': '100',
+            'D': '500',
+            'M': '1000'
+        }
+        sum = 0
+        #print(s)
+        for i in range(len(s)):
+            if i < len(s)-1:
+                if int(change.get(s[i]))>=int(change.get(s[i+1])):
+                    sum = sum + int(change.get(s[i]))
+                else:
+                    sum = sum - int(change.get(s[i]))
+            else:
+                sum = sum + int(change.get(s[i]))
+
+        print(sum)
+
+r = Solution()
+r.romanToInt('XL')
