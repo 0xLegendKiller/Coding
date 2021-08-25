3799bbaa6ef8d08de9de9532590c5ffabaa3b3ef
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2021-08-25 11:37:17 +0530

    Create r_l_strip.py

---
+++ Self_Practice/r_l_strip.py
@@ -0,0 +1,14 @@
+PS D:\Machines\Languages\VSC> & C:/Users/Shivam/AppData/Local/Programs/Python/Python38/python.exe
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
+# strip spaces from right side
+>>> res = "Adi    "
+>>> res.rstrip()
+'Adi'
+>>>  
+>>> res = "     Adi"     
+# strip spaces from left side
+>>> res.lstrip()     
+'Adi'
+>>>  
