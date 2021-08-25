12841169d4815ca97a032d7c96c8a583e76ace39
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2021-08-25 11:21:34 +0530

    Update and rename argparse.py to argparsePositionalArgs.py

--- Self_Practice/argparsePositionalArgs.py
+++ Self_Practice/argparsePositionalArgs.py
@@ -1,18 +1,3 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("number1", help="Number 1 Here!")
-parser.add_argument("number2", help="Number 2 Here!")
-parser.add_argument("operations", help="Operations to perform")
-args = parser.parse_args()
-# print(args)
-print(args.number1)
-print(args.number2)
-print(args.operations)
-a = int(args.number1)
-b = int(args.number2)
-print(a + b)
-
 # PS D:\Machines\Languages\VSC> & C:/Users/Shivam/AppData/Local/Programs/Python/Python38/python.exe d:/Machines/Languages/VSC/tmp.py -h     
 # usage: tmp.py [-h] number1 number2 operations
 
@@ -30,5 +15,18 @@ print(a + b)
 # add
 # 9
 # PS D:\Machines\Languages\VSC> 
-  
-  
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("number1", help="Number 1 Here!")
+parser.add_argument("number2", help="Number 2 Here!")
+parser.add_argument("operations", help="Operations to perform")
+args = parser.parse_args()
+# print(args)
+print(args.number1)
+print(args.number2)
+print(args.operations)
+a = int(args.number1)
+b = int(args.number2)
+print(a + b)
