c3ae98c44d27b3aceee5d0c4c93890ec8e313389
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2021-08-25 11:20:48 +0530

    Create argparseOptionalArgs.py

---
+++ Self_Practice/argparseOptionalArgs.py
@@ -0,0 +1,44 @@
+# PS D:\Machines\Languages\VSC> & C:/Users/Shivam/AppData/Local/Programs/Python/Python38/python.exe d:/Machines/Languages/VSC/tmp.py --number1 4 --number2 5 --operations add      
+# 4
+# 5
+# add
+# 9
+# PS D:\Machines\Languages\VSC> & C:/Users/Shivam/AppData/Local/Programs/Python/Python38/python.exe d:/Machines/Languages/VSC/tmp.py --number1 4 --number2 5 --operations multi    
+# 4
+# 5
+# multi
+# 20
+# PS D:\Machines\Languages\VSC> & C:/Users/Shivam/AppData/Local/Programs/Python/Python38/python.exe d:/Machines/Languages/VSC/tmp.py --number1 4 --number2 5
+# 4
+# 5
+# None
+# Goodbye!
+# PS D:\Machines\Languages\VSC>
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# adding -- makes aruguments optional
+parser.add_argument("--number1", help="Number 1 Here!")
+parser.add_argument("--number2", help="Number 2 Here!")
+parser.add_argument("--operations", help="Operations to perform")
+
+args = parser.parse_args()
+# print(args)
+print(args.number1)
+print(args.number2)
+print(args.operations)
+
+# build a normal calc 
+a = int(args.number1)
+b = int(args.number2)
+
+if args.operations == "add":
+    print(a + b)
+elif args.operations == "subs":
+    print(a - b)
+elif args.operations == "multi":
+    print(a * b)
+else:
+    print("Goodbye!")
